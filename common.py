2a35d3989aae57a3a9b5ae53469e4586f6227d29
colinmorris <colin.morris2@gmail.com>
2017-03-22 14:34:43 -0700

    One big canonical song frame

--- common.py
+++ common.py
@@ -4,6 +4,10 @@ import os
 CHARTDATA_FILENAME = 'hot-100-chartdata.pickle'
 DB_FILENAME = 'hot-100.pickle'
 LYRICS_DIR = 'lyrics'
+OMNI_PICKLE_NAME = 'omnisongs.pickle'
+
+class NotScrapedException(Exception):
+    pass
 
 def have_lyrics(song):
     k = song_key(song)
@@ -20,13 +24,25 @@ def get_songdb():
         db = pickle.load(f)
     return db
 
+def get_omnisong():
+    with open(OMNI_PICKLE_NAME) as f:
+        om = pickle.load(f)
+    return om
+
 def get_chartdata():
     with open(CHARTDATA_FILENAME) as f:
         cd = pickle.load(f)
     return cd
 
-def get_sizes(k):
+def get_sizes(song_or_key):
+    if isinstance(song_or_key, basestring):
+        k = song_or_key
+    else:
+        k = song_key(song_or_key)
     path = os.path.join(LYRICS_DIR, k+'.txt')
-    raw = os.path.getsize(path)
+    try:
+        raw = os.path.getsize(path)
+    except OSError:
+        raise NotScrapedException
     comp = os.path.getsize(path+'.gz')
     return (raw, comp)
---
+++ god_frame.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+import common
+
+db = common.get_songdb()
+#columns=['artist', 'title', 'date', 'peak', 'scraped', 'raw', 'comp', ]
+rows = []
+for artist_discog in db.itervalues():
+    for song in artist_discog.itervalues():
+        try:
+            raw, comp = common.get_sizes(song)
+            scraped = True
+        except common.NotScrapedException:
+            raw = comp = None
+            scraped = False
+
+        row = dict(artist=song.artist, title=song.title, date=song.earliest,
+                peak=song.peakPos, scraped=scraped, raw=raw, comp=comp)
+        rows.append(row)
+
+df = pd.DataFrame(rows)
+print "Saving god frame with shape {}".format(df.shape)
+df.to_pickle(common.OMNI_PICKLE_NAME)
